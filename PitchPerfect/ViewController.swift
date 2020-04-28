8362286b7a9ec1cbdcdc51603431242d4175d288
Dylan Carlson <dylan.dylancarlson@gmail.com>
2020-04-27 19:06:35 -0600

    adding button imgs and more functionality

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,19 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad called")
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         print("stop recording")
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordingButton.isEnabled = true
     }
 }
 
